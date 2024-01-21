870730cfdfa3ccfab8a0aacb681de3e339aa3c9c
Necro- <necrotto@gmail.com>
2024-01-21 05:16:02 +0200

    style: finished the styling for the display and start button.

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,4 +1,7 @@
-﻿const StartButton = ({ callback }) => (
-    <div>Start Button</div>
+﻿import React from 'react'
+import {StyledStartButton} from './styles/StyledStartButton'
+
+const StartButton = ({ callback }) => (
+    <StyledStartButton onClick={callback}>Start Button</StyledStartButton>
 )
 export default StartButton
